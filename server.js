2b32343de7bf3fb7696a020780152a6681b20c35
Aritra Dutta <70317724+aritradutta2001@users.noreply.github.com>
2024-04-17 01:18:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,5 +26,6 @@ app.use('/api/admin',adminRoutes);
 
 // server
 app.listen(port, () => {
-    console.log('Server is running on port http://localhost:' + port);
+    // console.log('Server is running on port http://localhost:' + port);
+    console.log('Server is running on' + https://exam-system-jaii.onrender.com);
 });
